f3f0d9a216d1c03b316aefce73de190e3ee6cc74
opussf <github@zz9-za.com>
2025-11-02 18:26:27 -0800

    Stubs Update

--- test/wowTest.lua
+++ test/wowTest.lua
@@ -1,7 +1,7 @@
 -----------------------------------------
 -- Author  :  Opussf
 -- Date    :  November 02 2025
--- Revision:  9.5.1-25-g3e910d1
+-- Revision:  9.5.2
 -----------------------------------------
 -- This is an uber simple unit test implementation
 -- It creates a dictionary called test.
